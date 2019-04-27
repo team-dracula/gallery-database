aa2343c8564805ec50e2cd934a273302e5847d62
Jackson <jacksonrcohen@gmail.com>
2019-04-27 13:49:55 -0400

    Fixed database query bug

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 const { getListingByID } = require('../database/utils.js');
-const port = process.env.PORT || 3010;
+const port = process.env.PORT || 3011;
 
 app.get('/api/:id', (req, res) => {
   getListingByID(req.params.id, (err, results) => {
     if (err) console.error('Error querying database', err);
-    else return results;
+    else res.send(results);
   });
 });
 
